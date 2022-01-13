6a05a656310f9b068dc399f6c126c3646b1c2289
Henry Patrick Karugendo <henry.p.karugendo@gmail.com>
2022-01-13 19:44:15 +0000

    c1 - Initial Commit, App Created With Dependencies

---
+++ src/main/java/com/hpkarugendo/spring_vue_app/SpringVueAppApplication.java
@@ -0,0 +1,13 @@
+package com.hpkarugendo.spring_vue_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVueAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hpkarugendo/spring_vue_app/SpringVueAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpkarugendo.spring_vue_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
